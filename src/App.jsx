8bfcad3b8ce86c48e0dbcaefee8a163982379933
ogunlade adebayo peter <ogunladeadebayopeter@gmail.com>
2024-01-29 13:25:45 -0100

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,23 @@ import About from './components/About';
 import Service from './components/Service';
 import Contact from './components/Contact';
 import Layout from './components/layout';
+import ContactDetails from './components/ContactDetails';
 
 function App() {
-
   return (
     <>
-    {/* <Navbar/> */}
-    <Routes>
-      <Route  element={<Layout/>}>
-     <Route path='/' element={<Home/>}/>
-     <Route path='/service' element={<Service/>}/>
-     <Route path='/about' element={<About/>}/>
-     <Route path='/contact' element={<Contact/>}/>
-     <Route path='/contact/dea' element={<Contact/>}/>
-
-     </Route>
-    </Routes>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route  element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/contact/details/:id' element={<ContactDetails />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
